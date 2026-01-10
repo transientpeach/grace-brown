a1a16d8d3e4ea1db7880443b0f3cddbecadc83b8
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-10 16:36:54 +0000

    mk src dir mv app dir update base styles and content

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Rubik, Inter_Tight } from "next/font/google";
+import "./globals.css";
+
+const interTight = Inter_Tight({
+  variable: "--font-inter-tight",
+  subsets: ["latin"],
+});
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Grace Brown",
+  description: "researcher and educator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${interTight.variable} ${rubik.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
