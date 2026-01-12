deaaac5a9d9922810b54957a66abb0805855ea4b
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-12 16:43:01 +0000

    teaching page init

---
+++ src/app/teaching/page.tsx
@@ -0,0 +1,26 @@
+const Teaching = () => {
+  return (
+    <div>
+      <h1>Teaching</h1>
+      <ul>
+        <li>
+          <h2>Course co-convenor</h2>
+          <p>Global Energy Politics (POLITIC4168)</p>
+        </li>
+
+        <li>
+          <h2>Graduate Teaching Assistant </h2>
+          <p>
+            Introduction to Alternative Perspectives in Economics (ECON1011)
+          </p>
+        </li>
+
+        <li>
+          <h2>Guest lectures and seminars</h2>
+          <p>Philosophy of Economics: On Community Wealth Building </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Teaching;
