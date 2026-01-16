be17518989b2adad916d2792804ce883c4d6c40f
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-16 17:56:36 +0000

    update header footer styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "@/components";
 export const Header = () => {
   return (
     <header className="w-full flex items-center justify-center">
-      <div className="w-full flex max-w-6xl px-16 ">
+      <div className="w-full flex max-w-4xl px-16 ">
         <Navigation />
       </div>
     </header>
