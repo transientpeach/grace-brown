26b16c741e5f4fbfcc5017705b0bce4676a4b216
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-18 19:43:22 +0000

    add style to custom link

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -4,13 +4,20 @@ import { AnchorHTMLAttributes } from "react";
 interface CustomLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
 }
-
-export const CustomLink = ({ href, children, ...props }: CustomLinkProps) => {
+// text-orange-300 focus:ring-orange-300
+const linkStyles =
+  "text-[#8b4513] dark:text-peach underline ring-offset-2 ring-offset-background hover:no-underline hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-[#8b4513]  dark:focus:ring-peach";
+export const CustomLink = ({
+  href,
+  className,
+  children,
+  ...props
+}: CustomLinkProps) => {
   const isInternal = href.startsWith("/") || href.startsWith("#");
 
   if (isInternal) {
     return (
-      <Link href={href} {...props}>
+      <Link href={href} className={`${linkStyles} ${className} `} {...props}>
         {children}
       </Link>
     );
@@ -18,6 +25,7 @@ export const CustomLink = ({ href, children, ...props }: CustomLinkProps) => {
 
   return (
     <a
+      className={`${linkStyles} ${className} `}
       href={href}
       target="_blank"
       rel="external noopener noreferrer"
